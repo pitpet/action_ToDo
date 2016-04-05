93d22a904acf9ffcabbd7820bc82936732806dc5
Peter Ruppen <peter.ruppen@gmail.com>
2016-04-05 22:01:53 +0200

    Added Toast Notification (Template)

--- actionToDo/MainPage.xaml.cs
+++ actionToDo/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,27 @@ namespace actionToDo
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock1.Text = "changed";
+
+            //Sending a toast notification
+
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastText01;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            //Change content
+            XmlNodeList toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode("Hello World!"));
+
+            //Create Toast
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            //Send Toast
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+
         }
     }
 }
--- actionToDo/ToDoElement.cs
+++ actionToDo/ToDoElement.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace actionToDo
 {
-    class ToDoElement
+    public class ToDoElement
     {
-        //a
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public string status { get; set; }
+        public int ID { get; set; }
+
     }
 }
